341741f88316a0d2a343ebf69abec2865880b0de
Алексей бондарчук <76469391+ABondarchu@users.noreply.github.com>
2021-12-26 01:03:46 +0300

    Update 1.cs

--- 1/1.cs
+++ 1/1.cs
@@ -4,8 +4,8 @@ namespace A
 {
     class A
     {
-        private float a = 18,9F;
-        private float b = 3,9F;
+        private float a = 18.4F;
+        private float b = 3.4F;
 
         public float c1
         {
