31ccf16809a70fcddfc3a5e07bca6b339421df45
Алексей бондарчук <76469391+ABondarchu@users.noreply.github.com>
2021-12-26 00:57:18 +0300

    Update 1.cs

--- 1/1.cs
+++ 1/1.cs
@@ -1,16 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace A
-{
- class Programm
- {
- static void Main(string[] args)
- {
- A aObject = new A();
-  Console.WriteLine($"Result: {aObject.c}");
-  }
-   }
+using  System;
+
+    public  class A
+    {
+        private float a = 18,9F;
+        private float b = 3,7F;
+
+        public float C0
+        {
+            get { return (a - b);}
+        }
+       public float C1
+        }
+        {
+           get { return a/b; }
+        }
+        }
 }
+
+     public  class Programm
+      
+    {
+        static void Main(string[] args)
+         
+        {
+            A aObject = new A();
+            Console.WriteLine($"Result: {aObject.c}");
+        }
+    }
