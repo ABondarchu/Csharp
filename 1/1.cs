68d5f2b1202b19335a4c611ae36547bea4128f80
Алексей бондарчук <76469391+ABondarchu@users.noreply.github.com>
2021-12-26 00:22:02 +0300

    Create 1.cs

---
+++ 1/1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace A
+{
+ class Programm
+ {
+ static void Main(string[] args)
+ {
+ A aObject = new A();
+  Console.WriteLine($"Result: {aObject.c}");
+  }
+   }
+}
