53ff10f143ede4c88288837713091960523b2134
Алексей бондарчук <76469391+ABondarchu@users.noreply.github.com>
2021-12-26 15:39:24 +0300

    Create 4.2.cs

---
+++ 4/4.2.cs
@@ -0,0 +1,64 @@
+using System;
+
+    namespace LB_2
+{
+    class B : A
+    {
+        private float d;
+        private float[] arr;
+        private float[,] array = { { 2, 3, 4 }, { 5, 6, 7 } };
+        private float cTwo;
+        public float this[int ind1, int ind2]
+        {
+             get { return array[ind1, ind2]; }
+             set { array[ind1, ind2] = value; }
+        }
+            public float this[int ind1]
+        {
+            get { return arr[ind1]; }
+            set { arr[ind1] = value; }
+        }
+
+
+        public B(float D, float A, float B) : base(A, B)
+        {
+            d = D;
+            cTwo = c2;
+        }
+
+        public B(float d, float a, float b, int coef) : this(d, a, b)
+        {
+            arr = new float[(int)a];
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = cTwo * i;
+            }
+        }
+
+        public void printArr()
+        {
+            int k = 0;
+            foreach (int i in arr)
+            {
+                Console.WriteLine($"arr [{k++}]: {i}");
+            }
+        }
+        public float c2
+        {
+            get 
+            {
+                switch(d)
+                {
+                    case 1:
+                        d /= a + b;
+                        break;
+                    default:
+                        d += a + b;
+                        break;
+                }
+                return d;
+            }
+        }
+    }
+}
